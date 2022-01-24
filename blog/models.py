0ad6db6d57cf235b3de0baf067dc26d59fc117b9
asatt <asattenojo99@gmail.com>
2022-01-24 15:28:56 +0900

    add search

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,41 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.db.models import Q
+from django.contrib.auth import get_user_model
 
+
+# class PostQuerySet(models.QuerySet):
+#     def search(self, query = None):
+#         print("serch")
+#         qs = self
+#         qs = qs.filter(published_date__lte = timezone.now())
+#
+#         if query is not None:
+#             or_lookup = (
+#                 Q(text__icontains = query)
+#             )
+#             qs = qs.filter(or_lookup).distinct()
+#         return qs.order_by('published_date')
+#
+# class PostManager(models.Manager):
+#     def get_queryset(self):
+#         return PostQuerySet(self.model, using = self._db)
+#
+#     def search(self, query = None):
+#         print('testtesttest',type(query))
+#         return self.get_queryset().search(self, query = query)
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    # shortText = models.TextField()
+    words = models.TextField(default = '')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    # objects = PostManager()
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,58 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib import messages
+from django.db.models import Q
+from functools import reduce
+from operator import and_
+
+counter = 0
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+def post_list(request, query = ''):
+    global counter
+    counter += 1
+    print(counter)
+    if request.method == "POST":
+        query = request.POST['q']
+        print(query)
+
+    words = parse_search_params(query)
+    if query != '':
+        or_lookup = reduce(and_,[(Q(text__icontains = p)|Q(words__icontains = p)|Q(title__icontains = p))&Q(published_date__lte = timezone.now()) for p in words])
+
+        posts = Post.objects.filter(or_lookup)
+    else:
+        posts = Post.objects.filter(published_date__lte = timezone.now())
+    # posts = Post.objects.filter(
+    #     Q(text__icontains = query)&Q(published_date__lte = timezone.now())
+    # )
+    posts = posts.order_by('published_date')
+    print('call post_list')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def parse_search_params(words: str):
+        search_words = words.replace('　', ' ').split()
+        return search_words
+
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    print('call post_new')
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit = False)
+            # if(len(post.text) > 50):
+            #     post.shortText = post.text[0:50]
+            # else:
+            #     post.shortText = post.text
+
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
@@ -29,11 +65,18 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
+    print('call post_edit')
     post = get_object_or_404(Post, pk = pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance = post)
         if form.is_valid():
             post = form.save(commit = False)
+            print(len(post.text))
+            # if(len(post.text) > 50):
+            #     post.shortText = post.text[0:50]
+            # else:
+            #     post.shortText = post.text
+
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
